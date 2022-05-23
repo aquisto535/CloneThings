e64cff57b700f5c15285cf54900025dd1e9f5968
Jung Tae Min <slrkaksemfdj@naver.com>
2022-05-23 22:38:35 +0900

    quiz-app day1
    let's start!

---
+++ quiz-app/script.js
@@ -0,0 +1,38 @@
+const quizData = [
+    {
+        question: 'How old are you?',
+        a: '10',
+        b: '20',
+        c: '24',
+        d: '26',
+        correct: 'd'
+    },{
+        question: 'what is the best programming language?',
+        a: 'Java',
+        b: 'C',
+        C: 'Phython',
+        d: 'Javascript',
+        correct: 'a'
+    }, {
+        question: 'who is the president of US?',
+        a: 'Biden',
+        b: 'Trump',
+        c: 'Obama',
+        d: 'clinton',
+        correct:'a'
+    }, {
+        question:'what does HTML stands for?',
+        a:'Markup language',
+        b: 'Cascading Style Sheet',
+        c: 'Jason Object Notation',
+        d: 'Application Programing Interface',
+        correct: 'a'
+    }, {
+        question: 'what year was Javascript launch?',
+        a: '2020',
+        b: '2021',
+        c: '2022',
+        d: '1994',
+        correct: 'd'
+    }
+]
