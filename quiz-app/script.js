75dd30cb59fcbe1ba4dc5d683ee3b8bb9903951a
Jung Tae Min <slrkaksemfdj@naver.com>
2022-05-27 22:37:20 +0900

    Quiz App Day4
    finished!

--- quiz-app/script.js
+++ quiz-app/script.js
@@ -37,6 +37,8 @@ const quizData = [
     }
 ]
 
+const quiz = document.getElementById('quiz')
+const answerEls = document.querySelectorAll(".answer");
 const questionEl = document.getElementById('question')
 const a_text = document.getElementById('a_text')
 const b_text = document.getElementById('b_text')
@@ -44,66 +46,69 @@ const c_text = document.getElementById('c_text')
 const d_text = document.getElementById('d_text')
 const submitBtn = document.getElementById('submit')
 
+
 let currentQuiz = 0;
 let score = 0;
 
 loadQuiz();
 
 function loadQuiz(){
-    const currentQuizData = quizData[currentQuiz]
+   
+
+    const currentQuizData = quizData[currentQuiz];
 
     questionEl.innerText = currentQuizData.question
-
-    a_text.innerText = currentQuizData.a
-    b_text.innerText = currentQuizData.b
-    c_text.innerText = currentQuizData.c
-    d_text.innerText = currentQuizData.d
+    
+    a_text.innerText = currentQuizData.a;
+    b_text.innerText = currentQuizData.b;
+    c_text.innerText = currentQuizData.c;
+    d_text.innerText = currentQuizData.d;
 
 
     
 }
 
-
-
 function getselected(){
-    const answerEls = document.querySelectorAll('.answer')
+    
 
     let answer = undefined
 
     answerEls.forEach((answerEl) =>{
-        if(answerEl.checked){
-            answer = answerEl.id
-        
-           }
-        
-        })
-       
-     return answer
-    }
-
-
-submitBtn.addEventListener('click', () =>{
-
-    const answer = getselected();
-
-    console.log(answer)
-
-    if(answer){
-
-        if(answer === quizData[currentQuiz].correct){
-            score++
+        if (answerEl.checked) {
+            answer = answerEl.id;
+            
         }
-        currentQuiz++
+        
+     
+    });
+    return answer;
+}
 
-        if(currentQuiz < quizData.length){
-            loadQuiz()
-    } else{
-            // show results
-        alert('you are finished!')
-    }
-    }
+
+
+    submitBtn.addEventListener("click", () => {
+        // check to see the answer
+        const answer = getselected();
+
+        
     
-
+        if (answer) {
+            if (answer === quizData[currentQuiz].correct) {
+                score++;
+            }
+    
+            currentQuiz++;
+            if (currentQuiz < quizData.length) {
+                loadQuiz();
+            } else {
+                quiz.innerHTML = `
+                    <h2>You answered correctly at ${score}/${quizData.length} questions.</h2>
+                    
+                    <button onclick="location.reload()">Reload</button>
+                `;
+            }
+        }
+    });
     
 
 
@@ -113,5 +118,3 @@ submitBtn.addEventListener('click', () =>{
 
   
     
-}
-)
