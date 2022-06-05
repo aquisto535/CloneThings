774647a76af8fe0bd0fb23b51d98a7374ae6df34
Jung Tae Min <slrkaksemfdj@naver.com>
2022-06-05 21:42:47 +0900

    Recipe App Day6
    UpperCase is very annoying me!

--- receipe-app/script.js
+++ receipe-app/script.js
@@ -48,8 +48,33 @@ function addMeal(mealData, random = false) {
 	const btn = meal.querySelector('.meal-body .fav-btn');
 
 	btn.addEventListener('click', () => {
-		btn.classList.toggle('active');
+		if (btn.classList.contains('active')) {
+			removeMealLS(mealData.idMeal);
+			btn.classList.remove('active');
+		} else {
+			addMealLS(mealData.idMeal);
+			btn.classList.add('active');
+		}
 	});
 
 	meals.appendChild(meal);
+}
+
+function addMealLS(mealId) {
+	const mealIds = getMealsLS();
+
+	localStorage.setItem('mealIds', JSON.stringify([...mealIds, mealId]));
+}
+function removeMealLS(mealId) {
+	const mealIds = getMealsLS();
+	localStorage.setItem(
+		'mealIds',
+		JSON.stringify(mealIds.filter((id) => id !== mealId))
+	);
+}
+
+function getMealsLS() {
+	const mealIds = JSON.parse(localStorage.getItem('mealIds'));
+
+	return mealIds === null ? [] : mealIds;
 }
