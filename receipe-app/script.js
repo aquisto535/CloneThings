3673c386d9724b26650af22bff3afa2fc3d77c28
Jung Tae Min <slrkaksemfdj@naver.com>
2022-06-06 23:51:09 +0900

    Recipe App Day 7
    I have no idea what I was wrong

--- receipe-app/script.js
+++ receipe-app/script.js
@@ -1,6 +1,7 @@
 const meals = document.getElementById('meals');
 
 getRandomMeal();
+fetchFavMeals();
 
 async function getRandomMeal() {
 	const resp = await fetch(
@@ -14,9 +15,14 @@ async function getRandomMeal() {
 }
 
 async function getMealById(id) {
-	const meal = await fetch(
+	const resp = await fetch(
 		'https://www.themealdb.com/api/json/v1/1/lookup.php?i=' + id
 	);
+
+	const respData = await resp.json();
+	const meal = respData.meals[0];
+
+	return meal;
 }
 
 async function getMealsBySearch(term) {
@@ -77,4 +83,20 @@ function getMealsLS() {
 	const mealIds = JSON.parse(localStorage.getItem('mealIds'));
 
 	return mealIds === null ? [] : mealIds;
+}
+
+async function fetchFavMeals() {
+	const mealIds = getMealsLS();
+
+	const meals = [];
+
+	for (let i = 0; i < mealIds.length; i++) {
+		const mealId = mealIds[i];
+
+		meal = await getMealById(mealId);
+
+		meals.push(meal);
+	}
+
+	// add them to the screen//
 }
