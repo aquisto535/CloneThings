73b9bae71d1f9ad345bbfca81ab480b2c138cff5
Jung Tae Min <slrkaksemfdj@naver.com>
2022-06-09 21:17:16 +0900

    Notes App Day2
    Don't give up!

--- notes-app/script.js
+++ notes-app/script.js
@@ -9,3 +9,11 @@ editBtn.addEventListener('click', () => {
 	main.classList.toggle('hidden');
 	textArea.classList.toggle('hidden');
 });
+
+textArea.addEventListener('input', (e) => {
+	const { value } = e.target;
+
+	main.innerHTML = marked(value);
+
+	console.log(main.innerHTML);
+});
