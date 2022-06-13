30c9bbab198aa6009e126fc1fb1e76c804c641d2
Jung Tae Min <slrkaksemfdj@naver.com>
2022-06-13 23:22:05 +0900

    Note App Day5
    The final is yet to come!

--- notes-app/script.js
+++ notes-app/script.js
@@ -1,10 +1,18 @@
 const addBtn = document.getElementById('add');
 
+const notes = JSON.parse(localStorage.getItem('notes'));
+
+if (notes) {
+	notes.forEach((note) => {
+		addNewNote(note);
+	});
+}
+
 addBtn.addEventListener('click', () => {
 	addNewNote();
 });
 
-function addNewNote() {
+function addNewNote(text = '') {
 	const note = document.createElement('div');
 	note.classList.add('note');
 
@@ -14,8 +22,8 @@ function addNewNote() {
 	<button class="edit"><i class="fas fa-edit"></i></button>
 	<button class="delete"><i class="fas fa-trash-alt"></i></button>
 	</div>
-	<div class="main hidden"></div>
-	<textarea></textarea>
+	<div class="main ${text ? '' : 'hidden'}"></div>
+	<textarea class=" ${text ? 'hidden' : ''}"></textarea>
 	`;
 
 	const editBtn = note.querySelector('.edit');
@@ -23,6 +31,9 @@ function addNewNote() {
 
 	const main = note.querySelector('.main');
 	const textArea = note.querySelector('textarea');
+
+	textArea.value = text;
+	main.innerHTML = marked(text);
 
 	editBtn.addEventListener('click', () => {
 		main.classList.toggle('hidden');
@@ -38,8 +49,20 @@ function addNewNote() {
 
 		main.innerHTML = marked(value);
 
-		console.log(main.innerHTML);
+		UpdateLS();
 	});
 
 	document.body.appendChild(note);
+}
+
+function UpdateLS() {
+	const notesText = document.querySelectorAll('textarea');
+
+	const notes = [];
+
+	notesText.forEach((note) => {
+		notes.push(note.value);
+	});
+
+	localStorage.setItem('notes', JSON.stringify(notes));
 }
