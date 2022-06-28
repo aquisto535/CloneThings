aa09fb055a63b006a9283efd88c63c67f9ede415
Jung Tae Min <slrkaksemfdj@naver.com>
2022-06-29 03:40:59 +0900

    github-profiles Day 3
    It's done!!

--- github-profiles/script.js
+++ github-profiles/script.js
@@ -6,17 +6,26 @@ const search = document.getElementById('search');
 
 getUser('florinpop17');
 
-async function getUser(user) {
-	const resp = await fetch(APIURL + user);
+async function getUser(username) {
+	const resp = await fetch(APIURL + username);
 	const respData = await resp.json();
 
 	createUserCard(respData);
+
+	getRepos(username);
+}
+
+async function getRepos(username) {
+	const resp = await fetch(APIURL + username + '/repos');
+	const respData = await resp.json();
+
+	addReposToCard(respData);
 }
 
 function createUserCard(user) {
 	const cardHTML = `
         <div class="card">
-            <div class=""img-container">
+            <div>
                 <img class="avatar" src="${user.avatar_url}" alt="${user.name}" />
             </div>
             <div class="user-info">
@@ -27,12 +36,36 @@ function createUserCard(user) {
                     <li>${user.following}<strong>Following</strong></li>
                     <li>${user.public_repos}<strong>Repos</strong></li>
                 </ul>
-                <div id="repos"></div>
+               
+            <h4>Repos:</h4>
+            <div id="repos"></div>                
+                
+                
             </div>
         </div>
     `;
 
 	main.innerHTML = cardHTML;
+}
+
+function addReposToCard(repos) {
+	const reposEl = document.getElementById('repos');
+
+	repos
+		.sort((a, b) => b.stargazers_count - a.stargazers_count) // sort: 정렬시키는 것(지금은 내림차순). 기본적으로는 오름차순임.
+		.slice(0, 10) // 10개만 나오게 만드는 것.
+		.forEach((repo) => {
+			const repoEl = document.createElement('a');
+			repoEl.classList.add('repo');
+
+			repoEl.href = repo.html_url;
+			repoEl.target = '_blank';
+			repoEl.innerText = repo.name;
+
+			reposEl.appendChild(repoEl);
+		});
+
+	console.log(repos);
 }
 
 form.addEventListener('submit', (e) => {
