570890de6569572e4915c3c73f158ff1c248235e
Jung Tae Min <slrkaksemfdj@naver.com>
2022-06-30 03:55:59 +0900

    drawing-app Day 1
    It's a 8th project! it will not take long!

---
+++ drawing-app/script.js
@@ -0,0 +1,30 @@
+const canvas = document.getElementById('canvas');
+
+const ctx = canvas.getContext('2d');
+
+let size = 30;
+let x = 50;
+let y = 50;
+
+canvas.addEventListener('mousedown', (e) => {
+	const x = e.offsetX;
+	const y = e.offsetY;
+
+	drawCircle(x, y);
+});
+
+function drawCircle(x, y) {
+	ctx.beginPath();
+	ctx.arc(x, y, size, 0, Math.PI * 2);
+	ctx.fill();
+}
+
+// function draw() {
+// 	ctx.clearRect(0, 0, canvas.Width, canvas.height);
+
+// 	drawCircle(x, y);
+
+// 	requestAnimationFrame(draw);
+// }
+
+// draw();
