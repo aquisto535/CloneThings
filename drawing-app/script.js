44a07b24befeb250cfb3837fc04daa928cb8386a
Jung Tae Min <slrkaksemfdj@naver.com>
2022-07-05 02:48:56 +0900

    drawing-app Day 2
    I am back!

--- drawing-app/script.js
+++ drawing-app/script.js
@@ -1,22 +1,56 @@
 const canvas = document.getElementById('canvas');
+const increaseBtn = document.getElementById('increase');
+const decreaseBtn = document.getElementById('decrease');
+const sizeEl = document.getElementById('size');
 
 const ctx = canvas.getContext('2d');
 
-let size = 30;
+let size = 20;
 let x = 50;
 let y = 50;
+let isPressed = false;
 
-canvas.addEventListener('mousedown', (e) => {
-	const x = e.offsetX;
-	const y = e.offsetY;
+canvas.addEventListener('mousedown', () => {
+	isPressed = true;
+});
 
-	drawCircle(x, y);
+canvas.addEventListener('mouseup', () => {
+	isPressed = false;
+});
+
+canvas.addEventListener('mousemove', (e) => {
+	if (isPressed) {
+		const x = e.offsetX;
+		const y = e.offsetY;
+
+		drawCircle(x, y);
+	}
 });
 
 function drawCircle(x, y) {
 	ctx.beginPath();
 	ctx.arc(x, y, size, 0, Math.PI * 2);
 	ctx.fill();
+}
+
+increaseBtn.addEventListener('click', () => {
+	size += 5;
+
+	if (size > 50) {
+		size = 50;
+	}
+	updateSize();
+});
+decreaseBtn.addEventListener('click', () => {
+	size -= 5;
+	if (size < 5) {
+		size = 5;
+	}
+	updateSize();
+});
+
+function updateSize() {
+	sizeEl.innerText = size;
 }
 
 // function draw() {
