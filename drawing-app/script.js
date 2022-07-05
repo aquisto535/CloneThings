5f4fba4f8c2028780ceb300f337357a3f660e763
Jung Tae Min <slrkaksemfdj@naver.com>
2022-07-06 04:03:33 +0900

    drawing-app Day 3
    I add my option!

--- drawing-app/script.js
+++ drawing-app/script.js
@@ -2,20 +2,28 @@ const canvas = document.getElementById('canvas');
 const increaseBtn = document.getElementById('increase');
 const decreaseBtn = document.getElementById('decrease');
 const sizeEl = document.getElementById('size');
+const colorEl = document.getElementById('color');
+const toolbox = document.getElementById('toolbox');
 
 const ctx = canvas.getContext('2d');
 
 let size = 20;
-let x = 50;
-let y = 50;
+let x = undefined;
+let y = undefined;
 let isPressed = false;
+let color = 'black';
 
-canvas.addEventListener('mousedown', () => {
+canvas.addEventListener('mousedown', (e) => {
 	isPressed = true;
+
+	x = e.offsetX;
+	y = e.offsetY;
 });
 
-canvas.addEventListener('mouseup', () => {
+canvas.addEventListener('mouseup', (e) => {
 	isPressed = false;
+	x = undefined;
+	y = undefined;
 });
 
 canvas.addEventListener('mousemove', (e) => {
@@ -31,6 +39,14 @@ function drawCircle(x, y) {
 	ctx.beginPath();
 	ctx.arc(x, y, size, 0, Math.PI * 2);
 	ctx.fill();
+	ctx.fillStyle = color;
+}
+
+function line(x1, y1, x2, y2) {
+	ctx.beginPath();
+	ctx.moveTo(x1, y1);
+	ctx.lineTo(x2, y2);
+	ctx.fillStyle = color;
 }
 
 increaseBtn.addEventListener('click', () => {
@@ -47,6 +63,11 @@ decreaseBtn.addEventListener('click', () => {
 		size = 5;
 	}
 	updateSize();
+});
+
+colorEl.addEventListener('change', (e) => {
+	color = e.target.value;
+	toolbox.style.backgroundColor = color;
 });
 
 function updateSize() {
