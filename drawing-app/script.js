77d5151da722a3bacefebfa0bfdff7d9cec35fc4
Tae Min Jung <71391013+aquisto535@users.noreply.github.com>
2022-08-01 23:02:13 +0900

    drawing-app Day 4
    it's finished!!

--- drawing-app/script.js
+++ drawing-app/script.js
@@ -3,6 +3,7 @@ const increaseBtn = document.getElementById('increase');
 const decreaseBtn = document.getElementById('decrease');
 const sizeEl = document.getElementById('size');
 const colorEl = document.getElementById('color');
+const clearEl = document.getElementById('clear');
 const toolbox = document.getElementById('toolbox');
 
 const ctx = canvas.getContext('2d');
@@ -28,25 +29,30 @@ canvas.addEventListener('mouseup', (e) => {
 
 canvas.addEventListener('mousemove', (e) => {
 	if (isPressed) {
-		const x = e.offsetX;
-		const y = e.offsetY;
+		const x2 = e.offsetX;
+		const y2 = e.offsetY;
 
-		drawCircle(x, y);
+		drawCircle(x2, y2);
+		drawLine(x, y, x2, y2);
+		x = x2;
+		y = y2;
 	}
 });
 
 function drawCircle(x, y) {
 	ctx.beginPath();
 	ctx.arc(x, y, size, 0, Math.PI * 2);
-	ctx.fill();
 	ctx.fillStyle = color;
+	ctx.fill();
 }
 
-function line(x1, y1, x2, y2) {
+function drawLine(x1, y1, x2, y2) {
 	ctx.beginPath();
 	ctx.moveTo(x1, y1);
 	ctx.lineTo(x2, y2);
-	ctx.fillStyle = color;
+	ctx.strokeStyle = color;
+	ctx.lineWidth = size;
+	ctx.stroke();
 }
 
 increaseBtn.addEventListener('click', () => {
@@ -68,18 +74,14 @@ decreaseBtn.addEventListener('click', () => {
 colorEl.addEventListener('change', (e) => {
 	color = e.target.value;
 	toolbox.style.backgroundColor = color;
+	toolbox.style.borderColor = color;
+	canvas.style.borderColor = color;
 });
 
 function updateSize() {
 	sizeEl.innerText = size;
 }
 
-// function draw() {
-// 	ctx.clearRect(0, 0, canvas.Width, canvas.height);
-
-// 	drawCircle(x, y);
-
-// 	requestAnimationFrame(draw);
-// }
-
-// draw();
+clearEl.addEventListener('click', () => {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
